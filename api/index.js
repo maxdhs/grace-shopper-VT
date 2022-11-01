a335165b37ff49134bb96ed23a7caca98e52ae18
maxwell dubin <maxdhs@gmail.com>
2022-11-01 12:07:30 -0400

    bookRouter

--- api/index.js
+++ api/index.js
@@ -1,9 +1,8 @@
 const express = require("express");
+const bookRouter = require("./books");
 
 const apiRouter = express.Router();
 
-apiRouter.get("/", (req, res) => {
-  res.send("api router working");
-});
+apiRouter.use("/books", bookRouter);
 
 module.exports = apiRouter;
